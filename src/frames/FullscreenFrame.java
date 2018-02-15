2be6a446c742e1284364b4aa3748fd44d644a728
flyingdutchman <mika.pagani@heig-vd.ch>
2018-02-15 23:55:59 +0100

    Can only close with command, ALT+F4 disabled

--- src/frames/FullscreenFrame.java
+++ src/frames/FullscreenFrame.java
@@ -8,13 +8,10 @@ public class FullscreenFrame extends JFrame{
     public FullscreenFrame() {
         super("");
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
         // Make it fullscreen
         setExtendedState(JFrame.MAXIMIZED_BOTH);
-        //setSize(new Dimension(200,200));
         setAlwaysOnTop(true);
         setUndecorated(true);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
     }
-
 }
